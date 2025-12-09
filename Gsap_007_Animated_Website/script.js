3f89bf897e9edc004e39407e287274f565b6d65d
unknown <manishrajkumar856@gmail.com>
2025-12-09 19:37:52 +0530

    Animation Completed

--- Gsap_007_Animated_Website/script.js
+++ Gsap_007_Animated_Website/script.js
@@ -34,3 +34,160 @@
 //   duration: 0.6,
 //   stagger: -0.3,
 // })
+
+function animation01(){
+    const tl = gsap.timeline();
+
+tl.from(".header-sec .logo",{
+    y: -30,
+    opacity: 0,
+    delay: 0.71,
+    duration: 0.5,
+
+})
+
+tl.from(".header-sec .links a, .header-sec .nav-btn",{
+    y: -30,
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.2,
+})
+
+
+tl.from(".hero-section .text-sec h1",{
+    x: -300,
+    opacity: 0,
+    duration: 0.5,
+})
+
+tl.from(".hero-section .text-sec p",{
+    x: -100,
+    opacity: 0,
+    duration: 0.4,
+})
+
+
+tl.from(".text-sec .btn-sec",{
+    x: -50,
+    opacity: 0,
+    duration: 0.4,
+})
+
+tl.from(".hero-section .img-sec",{
+    x: 50,
+    opacity: 0,
+    duration: 0.5,
+}, "-=1")
+
+}
+
+
+function animation02(){
+    tl2 = gsap.timeline({
+        scrollTrigger:{
+            trigger: ".sec-01",
+            scroller: "body",
+            start: "top 60%",
+            end: "top -50%",
+            markers: true,
+            scrub: 1,
+        },
+        stagger: 0.5,
+    })
+
+    tl2.from(".sec-01 .ser-text-sec",{
+        y: -50,
+        duration: 0.4,
+        opacity: 0,
+
+    })
+
+    tl2.from(".sec-01 .servics-container .line1.left",{
+        x: -200,
+        duration: 1,
+        opacity: 0,
+    },"elem1")
+
+    tl2.from(".sec-01 .servics-container .line1.right",{
+        x: 200,
+        duration: 1,
+        opacity: 0,
+    },"elem1")
+
+    tl2.from(".sec-01 .servics-container .line2.left",{
+        x: -200,
+        duration: 1,
+        opacity: 0,
+    },"elem2")
+
+    tl2.from(".sec-01 .servics-container .line2.right",{
+        x: 200,
+        duration: 1,
+        opacity: 0,
+    },"elem2")
+
+
+    tl2.from(".sec-01 .full-box-cont h3, .sec-01 .full-box-cont p",{
+        x: -50,
+        opacity: 0,
+        stagger: 0.2,
+    })
+
+    
+    tl2.from(".sec-01 .full-box-cont .btn-sec",{
+        x: -50,
+        opacity: 0,
+        stagger: 0.2,
+        scrollTrigger:{
+            trigger: ".sec-01 button",
+            scroller: "body",
+            start: "top 60%",
+            end: "top 70%",
+            markers: true,
+            scrub: 2,
+        }
+    },"-=1")
+
+    tl2.from(".sec-01 .full-box-cont img",{
+        x: 50,
+        opacity: 0,
+        stagger: 0.2,
+    })
+}
+
+function animation03(){
+    tl3 = gsap.timeline({
+        scrollTrigger:{
+            trigger: ".sec-03 .ser-text-sec",
+            scroller: "body",
+            
+            start: "top 60%",
+            end: "top: 40%",
+            scrub: 2,
+        },
+    })
+
+    tl3.from(".sec-03 .ser-text-sec",{
+        y: -50,
+        opacity: 0,
+        duration: 1,
+    })
+
+    tl3.from(".sec-03 .text-div p",{
+        Y: -50,
+        opacity: 0,
+        duration: 1,
+        stagger:1,
+    })
+
+    tl3.from(".sec-03 .text-div",{
+        x: -90,
+        opacity: 0,
+        duration: 0.5,
+    })
+
+}
+
+animation01();
+animation02();
+animation03();
